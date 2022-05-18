4b7a638400f8ad1edead40acabdd3ba681659c66
olefine921 <101977503+olefine921@users.noreply.github.com>
2022-05-18 12:40:03 +0200

    Update Measurement.py

--- Measurement.py
+++ Measurement.py
@@ -1,31 +1,30 @@
-from pymodbus.client.sync import ModbusSerialClient  # Import the pymodbus library part for syncronous master (=client)
-from pymodbus.payload import BinaryPayloadDecoder
-from time import time, ctime, sleep #import the time library
+from pymodbus.client.sync import ModbusSerialClient  #import the pymodbus library part for syncronous master (=client)
+from pymodbus.payload import BinaryPayloadDecoder #import the BinaryPayloadDecoder to translate signals
+from time import time, ctime, sleep #import the time  & datetime libraries
 from datetime import datetime, date, timedelta
-import matplotlib.pyplot as plt
-import numpy as np
-import csv #Import the csv library
+import numpy as np #import numpy for calculations, dash_usage.py uses pandas for calculation
+import csv #import the csv library for documentation
 
 client = ModbusSerialClient(
-    method='rtu',  # Modbus Modus = RTU = via USB & RS485
-    port='/dev/ttyUSB0',  # Connected over ttyUSB0, not AMA0
-    baudrate=19200,  # Baudrate was changed from 38400 to 19200
-    timeout=3,  #
-    parity='N',  # Parity = None
-    stopbits=2,  # Bites was changed from 1 to 2
-    bytesize=8  #
+    method='rtu',  #Modbus Modus = RTU = via USB & RS485
+    port='/dev/ttyUSB0',  #Connected over ttyUSB0, not AMA0
+    baudrate=19200,  #Baudrate was changed from 38400 to 19200
+    timeout=3,  #imeout after 3 sec of unanswered calls
+    parity='N',  #Parity = None
+    stopbits=2,  #Bites was changed from 1 to 2
+    bytesize=8  #Register size is 8 bytes
 )
-# Counter
+#Counter
 counter1 = 0 #for median loop
 counter2 = 1 #for counting the days -> 5760 = 4days
-counter3 = 0 #for making and saving 5 plots for the diashow
-# Variables
+
+#Variables
 t = 0
 
 pCO2 = 0
 temp = 0
 mbar = 0
-DLI = 0
+DLI = 0 #fourth register reads now mg/l, for less mistakes during coding, the old name has been left
 
 summePCO2 = []
 summeTemp = []
@@ -42,16 +41,13 @@ second_reading = 0
 third_reading = 0
 fourth_reading = 0
 
-#variables only for plotting
+#variables only for time
 startTime = datetime.now()
 loopedTime = 0
 csvTimeCounter = 0
 
-date = []
-x =[]
-plotAvPCO2 = []
-plotAvTemp = []
 
+#create list for line in csv & adresse of csv file, including path
 csvRow = []
 
 adresseTime = datetime.now().strftime('%Y.%m.%d')
@@ -63,6 +59,7 @@ with open(adresse + '.csv', 'w') as file:
     writer = csv.writer(file)
 
 #get input from calibration
+#important: use correct data type
 slope = float(input('Give the slope from the calibration as a float and confirm with Enter:   '))    
 intercept = float(input('Give the intercept from the calibration as a float and confirm with Enter:    '))
 rSq = float(input('Give the R^2 from the calibration as a float and confirm with Enter:    '))
@@ -74,9 +71,11 @@ while counter1 < 8:
         #reading from a holding register
         #startregister = 100, registers to be read = 8, answer size = 1 byte
         res = client.read_holding_registers(address=100, count=8, unit=1)
-        #creat decoder for res: '>' = Big Endian
+        
+        #define decoder for res (registers to be read) to get the organisation for big endian
+        #important: big endian = '>' 
         decoder = BinaryPayloadDecoder.fromRegisters(res.registers, byteorder='>', wordorder='>')
-        #use decoder to get 32bit float big endian
+        #use decoder to get values in 32-bit float big endian
         first_reading = decoder.decode_32bit_float()
         second_reading = decoder.decode_32bit_float()
         third_reading = decoder.decode_32bit_float()
@@ -88,11 +87,11 @@ while counter1 < 8:
             summeTemp.append(second_reading)
             summembar.append(third_reading)
             summeDLI.append(fourth_reading)
+            
             #add 1 to counter for median loop
             counter1 += 1
 
-            #after 1 min do this
-            if counter1 == 6:
+            if counter1 == 6: #after 1 min do this
                     
                 #calculate median for 1 min
                 avPCO2 = np.median(summePCO2)
@@ -102,14 +101,16 @@ while counter1 < 8:
 
                 #add calibration to pCO2
                 avPCO2 = avPCO2 * slope + intercept
-
-
+                
+                #get times to fill csv file
                 t = time()
                 dateForCSV = ctime(t)
                 
                 loopedTime = datetime.now()
                 csvTimeCounter = loopedTime - startTime
 
+                #add data to the list for writing into the csv file
+                #importan: order has to be the same as referenced in dash_usage.py
                 csvRow.append(dateForCSV)
                 csvRow.append(csvTimeCounter)
                 csvRow.append(avPCO2)
@@ -117,6 +118,7 @@ while counter1 < 8:
                 csvRow.append(avmbar)
                 csvRow.append(avDLI)
 
+                #write the data in the csv file
                 with open(adresse +'.csv', 'a') as file:
 
                     writer = csv.writer(file)
@@ -125,9 +127,7 @@ while counter1 < 8:
                     
 
 
-                sleep(7)  # Stops Loop for 7sec
-                
-
+                sleep(7)  # Stops Loop for 7sec, changed from 10sec to 7sec to keep overall length at 60sec
 
                 print('----------------------------------------------------------------')
 
@@ -154,18 +154,20 @@ while counter1 < 8:
                 loopedTime = 0
                 csvTimeCounter = 0
 
+                #print information for user, how long is the code running now, counter2 gets reset after 4 days
                 print('Code is running for ' + str(counter2) + ' minutes.')
 
                 if counter2 == 5760: #check if 4 days are over
-                    #set time variables for plot and csv-file back
-                    
+                    #set time variables for plot and csv-file back                 
                     divider = []
+                    #create lines to write calibration information at end of csv file
                     headerCalculations = ["Slope", "Intercept", "R^2"]
                     csvRow = []
                     csvRow.append(slope)
                     csvRow.append(intercept)
                     csvRow.append(rSq)
                     
+                    #write calibration information at end of csv file
                     with open(adresse + '.csv', 'a') as file:
 
                         writer = csv.writer(file)
@@ -173,14 +175,11 @@ while counter1 < 8:
                         writer.writerow(headerCalculations)
                         writer.writerow(csvRow)
                     
+                    #set variables back
                     startTime = datetime.now()
                     loopedTime = 0
                     csvTimeCounter = 0
 
-                    date = []
-                    x = []
-                    plotAvPCO2 = []
-                    plotAvTemp = []
                     csvRow = []
                     counter2 = 0
 
@@ -193,20 +192,21 @@ while counter1 < 8:
 
             else:
 
-                sleep(10)  # Stops Loop for 10sec
+                sleep(10)  #Stops Loop for 10sec
                 print('')
 
         else:
             print(res)  # Print Error Message, for meaning look at (insert git hub)
-            counter1 = 0
+            counter1 = 0 #set counter1 = 0 to keep loop going
             sleep(10)  # Stops Loop for 10sec
 
     else:  # If not able to connect, do this
+        #show in terminal to inform user
         print('Cannot connect to the Transmitter M80 SM and Sensor InPro 5000i.')
         print('----------------------------------------------------------------')
         print('Please check the following things:')
         print('Does the RS485-to-USB Adapter have power? Which LEDs are active?')
         print('Are the cables connected correctly?')
 
-        counter1 = 0
+        counter1 = 0 #set counter1 = 0 to keep loop going
         sleep(10)  # Stops Loop for 10sec
